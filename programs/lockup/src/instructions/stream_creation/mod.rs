cf8c3a41cedd484a5e987a74041058588dadbc10
Andrei Vlad Birgaoanu <99738872+andreivladbrg@users.noreply.github.com>
2025-04-08 14:22:23 +0300

    feat: implement create with duration function (#20)

--- programs/lockup/src/instructions/stream_creation/mod.rs
+++ programs/lockup/src/instructions/stream_creation/mod.rs
@@ -1,5 +1,7 @@
+pub mod create_with_durations;
 pub mod create_with_timestamps;
 pub mod prepare_for_stream_creation;
 
+pub use create_with_durations::*;
 pub use create_with_timestamps::*;
 pub use prepare_for_stream_creation::*;
