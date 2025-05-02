48fc56b7750939f0553fb775855c8ac916222932
Iaroslav Mazur <iaroslav.mazur@proton.me>
2025-05-02 04:46:43 -0700

    Iaro/view-ixs (#80)
    * feat: RefundableAmountOf Ix
    
    * depleted status
    
    * refactor: move common view context file to its file
    
    * refactor: rename the common context
    
    * refactor: rename view context
    
    ---------
    
    Co-authored-by: Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>

---
+++ programs/lockup/src/instructions/view/mod.rs
@@ -0,0 +1,8 @@
+pub mod refundable_amount_of;
+pub mod status_of;
+pub mod stream_view;
+pub mod streamed_amount_of;
+pub mod withdrawable_amount_of;
+
+pub use status_of::*;
+pub use stream_view::*;
---
+++ programs/lockup/src/instructions/view/status_of.rs
@@ -0,0 +1,41 @@
+use anchor_lang::prelude::*;
+
+use super::StreamView;
+use crate::utils::lockup_math::get_streamed_amount;
+
+pub fn handler(ctx: Context<StreamView>, _stream_id: u64) -> Result<StreamStatus> {
+    let stream_data = &ctx.accounts.stream_data;
+
+    if stream_data.is_depleted {
+        return Ok(StreamStatus::Depleted);
+    }
+
+    if stream_data.was_canceled {
+        return Ok(StreamStatus::Canceled);
+    }
+
+    // Get the current time
+    let current_time = Clock::get()?.unix_timestamp;
+
+    if current_time < stream_data.timestamps.start_time {
+        return Ok(StreamStatus::Pending);
+    }
+
+    // Calculate the streamed amount.
+    let streamed_amount = get_streamed_amount(&stream_data.timestamps, &stream_data.amounts);
+
+    if streamed_amount < stream_data.amounts.deposited {
+        Ok(StreamStatus::Streaming)
+    } else {
+        Ok(StreamStatus::Settled)
+    }
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize)]
+pub enum StreamStatus {
+    Pending,
+    Streaming,
+    Settled,
+    Canceled,
+    Depleted,
+}
