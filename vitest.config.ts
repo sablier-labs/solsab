d076eb239aa2ba67f484338899728617926f7742
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-07-26 16:18:48 +0300

    chore: include reporters in vitest config
    closes #193

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     globals: true,
     hookTimeout: 1_000_000, // 1000 seconds
     include: ["tests/**/*.test.ts"],
+    reporters: ["verbose"],
     testTimeout: 1_000_000, // 1000 seconds
   },
 });
