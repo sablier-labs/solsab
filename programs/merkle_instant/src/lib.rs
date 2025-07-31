27f71e69dedf3a45eb6e72ebea62b35e92e41294
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-07-31 16:53:23 +0300

    docs: polish

--- programs/merkle_instant/src/lib.rs
+++ programs/merkle_instant/src/lib.rs
@@ -20,7 +20,7 @@ pub mod sablier_merkle_instant {
     /// Claim airdrop on behalf of eligible recipient and transfer it to the recipient ATA.
     ///
     /// Notes:
-    /// - The instruction charges a fee in SOL equivalent to $2.
+    /// - The instruction charges a fee in the native token (SOL), equivalent to $2 USD.
     /// - Emits a {Claim} event.
     ///
     /// Accounts expected:
