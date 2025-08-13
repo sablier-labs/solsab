7a71a657c1b6e5f7a706457919212ec83821adec
Iaroslav Mazur <iaroslav.mazur@proton.me>
2025-08-13 18:36:06 +0300

    scripts: pr review

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   test: {
     environment: "node",
     globals: true,
     hookTimeout: 10_000, // 10 seconds
-    include: ["tests/**/*.test.ts"],
+    include: mode === "scripts" ? ["scripts/ts/init*.ts"] : ["tests/**/*.test.ts"],
     reporters: ["verbose"],
-    testTimeout: 10_000, // 10 seconds
+    testTimeout: mode === "scripts" ? 30_000 : 10_000, // 10 or 30 seconds, depending on the mode
   },
-});
+}));
