513ac043ef899d777d31063d4b262a6b613db680
Iaroslav Mazur <iaroslav.mazur@proton.me>
2025-08-18 14:28:42 +0300

    docs: pr review

--- programs/merkle_instant/src/utils/time.rs
+++ programs/merkle_instant/src/utils/time.rs
@@ -1,6 +1,6 @@
 use anchor_lang::{prelude::*, solana_program::sysvar::clock::Clock};
 
-/// Returns the current time in seconds.
+/// Returns the current time in seconds, as a `u64`.
 pub fn get_current_time() -> Result<u64> {
     Ok(Clock::get()?.unix_timestamp as u64) // The Unix timestamp is assumed to always be positive.
 }
