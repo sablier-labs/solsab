1ca7ff111ca0582d21972c87b229ff215c15cbe5
Andrei Vlad Birgaoanu <andreivladbrg@gmail.com>
2025-09-09 02:17:38 +0300

    fix: init scripts

--- scripts/ts/init-lockup.ts
+++ scripts/ts/init-lockup.ts
@@ -2,12 +2,12 @@ import * as anchor from "@coral-xyz/anchor";
 import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
 import { type Keypair } from "@solana/web3.js";
 import { describe, it } from "vitest";
-import { ProgramId } from "../../lib/constants";
+import { ProgramId, SABLIER_ADMIN } from "../../lib/constants";
 import { type SablierLockup } from "../../target/types/sablier_lockup";
 
 export let anchorProvider: anchor.AnchorProvider;
 export let lockupProgram: anchor.Program<SablierLockup>;
-export let senderKeys: Keypair;
+export let signerKeys: Keypair;
 
 describe("Sablier Lockup post-deployment initialization", () => {
   it("Initializes the Sablier Lockup program", async () => {
@@ -25,15 +25,15 @@ export async function configureTestingEnvironment() {
   anchor.setProvider(anchorProvider);
   lockupProgram = anchor.workspace.SablierLockup as anchor.Program<SablierLockup>;
   // Initialize the accounts involved in the tests
-  senderKeys = (anchorProvider.wallet as anchor.Wallet).payer;
+  signerKeys = (anchorProvider.wallet as anchor.Wallet).payer;
 }
 
 export async function initSablierLockup() {
   await lockupProgram.methods
-    .initialize(senderKeys.publicKey, ProgramId.CHAINLINK_PROGRAM, ProgramId.CHAINLINK_SOL_USD_FEED)
-    .signers([senderKeys])
+    .initialize(SABLIER_ADMIN, ProgramId.CHAINLINK_PROGRAM, ProgramId.CHAINLINK_SOL_USD_FEED)
+    .signers([signerKeys])
     .accounts({
-      initializer: senderKeys.publicKey,
+      initializer: signerKeys.publicKey,
       nftTokenProgram: TOKEN_PROGRAM_ID,
     })
     .rpc();
--- scripts/ts/init-merkle-instant.ts
+++ scripts/ts/init-merkle-instant.ts
@@ -1,12 +1,12 @@
 import * as anchor from "@coral-xyz/anchor";
 import { type Keypair } from "@solana/web3.js";
 import { describe, it } from "vitest";
-import { ProgramId } from "../../lib/constants";
+import { ProgramId, SABLIER_ADMIN } from "../../lib/constants";
 import type { SablierMerkleInstant } from "../../target/types/sablier_merkle_instant";
 
 export let anchorProvider: anchor.AnchorProvider;
 export let merkleInstantProgram: anchor.Program<SablierMerkleInstant>;
-export let campaignCreatorKeys: Keypair;
+export let signerKeys: Keypair;
 
 describe("Sablier Merkle Instant post-deployment initialization", () => {
   it("Initializes the Sablier Merkle Instant program", async () => {
@@ -26,15 +26,15 @@ export async function configureTestingEnvironment() {
   merkleInstantProgram = anchor.workspace.SablierMerkleInstant as anchor.Program<SablierMerkleInstant>;
 
   // Initialize the accounts involved in the tests
-  campaignCreatorKeys = (anchorProvider.wallet as anchor.Wallet).payer;
+  signerKeys = (anchorProvider.wallet as anchor.Wallet).payer;
 }
 
 export async function initSablierMerkleInstant() {
   await merkleInstantProgram.methods
-    .initialize(campaignCreatorKeys.publicKey, ProgramId.CHAINLINK_PROGRAM, ProgramId.CHAINLINK_SOL_USD_FEED)
-    .signers([campaignCreatorKeys])
+    .initialize(SABLIER_ADMIN, ProgramId.CHAINLINK_PROGRAM, ProgramId.CHAINLINK_SOL_USD_FEED)
+    .signers([signerKeys])
     .accounts({
-      initializer: campaignCreatorKeys.publicKey,
+      initializer: signerKeys.publicKey,
     })
     .rpc();
 }
